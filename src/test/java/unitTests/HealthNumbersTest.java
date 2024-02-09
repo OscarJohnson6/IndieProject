251b80c3f31e7b5a87b9dc9e268df67d12f33280
Oscar <ojohnson6@madisoncollege.edu>
2024-02-08 20:35:20 -0600

    Junit class setup so that it can be added upon later

---
+++ src/test/java/unitTests/HealthNumbersTest.java
@@ -0,0 +1,26 @@
+package unitTests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HealthNumbersTest {
+
+
+    /**
+     * Create a new InverseCaptcha object before each test is run.
+     */
+    @BeforeEach
+    void setUp() {
+    }
+
+    /**
+     * Determine the sum for your puzzle input, which is passed in as a file location
+     */
+    @Test
+    void determineInverseCaptchaForYourPuzzleInput() {
+        //assertEquals(0, 1);
+    }
+}
