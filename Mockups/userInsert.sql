c7a7b988766fd5bd540e8a250cf612e3f8cdedb1
Oscar <ojohnson6@madisoncollege.edu>
2024-02-11 12:03:47 -0600

    MySQL database insert statements for table user_profiles

---
+++ Mockups/userInsert.sql
@@ -0,0 +1,7 @@
+# INSERTs for the table user_profiles in the schema fit_app
+insert into user_profiles (userEmail, firstName, lastName) values ('joecoy@email.com', 'Joe', 'Coyne');
+insert into user_profiles (userEmail, firstName, lastName) values ('frensen@email.com', 'Fred', 'Hensen');
+insert into user_profiles (userEmail, firstName, lastName) values ('barry@email.com', 'Barney', 'Curry');
+insert into user_profiles (userEmail, firstName, lastName) values ('maren@email.com', 'Karen', 'Mack');
+insert into user_profiles (userEmail, firstName, lastName) values ('cleandine@email.com', 'Dianne', 'Klein');
+insert into user_profiles (userEmail, firstName, lastName) values ('tilldawn@email.com', 'Dawn', 'Tillman');
