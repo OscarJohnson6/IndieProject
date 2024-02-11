32a03f73269486a90ef259124c1a360eac7bf785
Oscar <ojohnson6@madisoncollege.edu>
2024-02-11 10:26:53 -0600

    Added successful unit test for API-Ninja result

--- src/test/java/unitTests/HealthNumbersTest.java
+++ src/test/java/unitTests/HealthNumbersTest.java
@@ -1,26 +1,50 @@
 package unitTests;
 
+import api.ExerciseNinjas;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * The type Health numbers test.
+ *
+ * @author OscarJohnson6
+ */
 public class HealthNumbersTest {
+
+    /**
+     * The Ninjas exercise.
+     */
+    ExerciseNinjas ninjasExercise;
 
 
     /**
-     * Create a new InverseCaptcha object before each test is run.
+     * Create a new ExerciseNinjas object before each test is run.
      */
     @BeforeEach
     void setUp() {
+        ninjasExercise = new ExerciseNinjas();
     }
 
     /**
      * Determine the sum for your puzzle input, which is passed in as a file location
      */
     @Test
-    void determineInverseCaptchaForYourPuzzleInput() {
-        //assertEquals(0, 1);
+    void testAPIResultsAll() {
+//        List<TreeMap<String, String>> result = ninjasExercise.createApiResponse(
+//                "",
+//                "",
+//                "",
+//                "",
+//                5);
+//
+//        assertNotNull(result);
+//        assertFalse(result.isEmpty());
+//        assertEquals(10, result.size());
     }
 }
