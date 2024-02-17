0de7776d6ce72fc3be3ffe6e187112bdc02a1cc8
Oscar <ojohnson6@madisoncollege.edu>
2024-02-17 14:22:01 -0600

    A copied properties loader interface to easily read properties files

---
+++ src/main/java/utilities/PropertiesLoader.java
@@ -0,0 +1,43 @@
+package utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/** Code copied from the course site for an interface to be implemented in the 
+ * analyzer project.
+ * 
+ * @author Eric Knapp
+ * @author OscarJohnson6
+ *
+ */
+public interface PropertiesLoader {
+
+    /**
+     * Object for logging any exception errors.
+     */
+    Logger logger = LogManager.getLogger(PropertiesLoader.class);
+
+    /**
+     * This default method will load a properties file into a Properties instance
+     * and return it.
+     * 
+     * @param propertiesFilePath    Location of file.
+     * @return                      The object variable properties final value.
+     * @since 1.0
+     */
+    default Properties loadProperties(String propertiesFilePath) {
+        Properties properties = new Properties();
+
+        try {
+            properties.load(this.getClass().getResourceAsStream(propertiesFilePath));
+        } catch (IOException ioException) {
+            logger.error("Error reading properties file ", ioException);
+        }
+        
+        return properties;
+    }
+
+}
