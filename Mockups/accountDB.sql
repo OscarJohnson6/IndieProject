319df4e5eec72a8bb4113cacc47e2a79b7f38deb
Oscar <ojohnson6@madisoncollege.edu>
2024-02-18 14:19:21 -0600

    New fit_app database MySQL set up creation

--- Mockups/accountDB.sql
+++ Mockups/accountDB.sql
@@ -1,4 +1,5 @@
 # MySQL table schema for indie project FitApp
+DROP SCHEMA IF EXISTS fit_app;
 
 create schema if not exists fit_app;
 
@@ -7,8 +8,8 @@ use fit_app;
 drop table if exists user_profiles;
 drop table if exists weight_records;
 drop table if exists height_records;
-drop table if exists neck_length_records;
-drop table if exists waist_length_records;
+drop table if exists hip_records;
+drop table if exists waist_records;
 
 #
 # TABLEs
@@ -16,9 +17,11 @@ drop table if exists waist_length_records;
 CREATE TABLE user_profiles (
                        userId INT AUTO_INCREMENT PRIMARY KEY,
                        userEmail VARCHAR(75) NOT NULL,
-                       firstName VARCHAR(25) NOT NULL,
-                       lastName VARCHAR(25) NOT NULL,
-                       gender VARCHAR(8) NOT NULL,
+                       firstName VARCHAR(25) NULL,
+                       lastName VARCHAR(25) NULL,
+                       gender VARCHAR(8) NULL,
+                       date_of_birth DATE NULL,
+                       activity_level VARCHAR(10) NULL,
                        entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -32,21 +35,21 @@ CREATE TABLE weight_records (
 CREATE TABLE height_records (
                                entryId INT AUTO_INCREMENT PRIMARY KEY,
                                userId INT NOT NULL,
-                               height INTEGER COMMENT 'As inches',
+                               height INTEGER NOT NULL COMMENT 'As cm',
                                entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE neck_length_records (
+CREATE TABLE hip_records (
                                    entryId INT AUTO_INCREMENT PRIMARY KEY,
                                    userId INT NOT NULL,
-                                   neck_length INTEGER COMMENT 'As inches',
+                                   hip INTEGER NOT NULL COMMENT 'As cm',
                                    entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE waist_length_records (
+CREATE TABLE waist_records (
                                     entryId INT AUTO_INCREMENT PRIMARY KEY,
                                     userId INT NOT NULL,
-                                    waist_length INTEGER COMMENT 'As inches',
+                                    waist INTEGER NOT NULL COMMENT 'As cm',
                                     entry_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -59,27 +62,30 @@ ALTER TABLE weight_records
 ALTER TABLE height_records
     ADD CONSTRAINT Userheight_fk FOREIGN KEY (userId)
         REFERENCES user_profiles (userId);
-ALTER TABLE neck_length_records
-    ADD CONSTRAINT Userneck_fk FOREIGN KEY (userId)
+ALTER TABLE hip_records
+    ADD CONSTRAINT Userhip_fk FOREIGN KEY (userId)
         REFERENCES user_profiles (userId);
-ALTER TABLE waist_length_records
+ALTER TABLE waist_records
     ADD CONSTRAINT Userwaist_fk FOREIGN KEY (userId)
         REFERENCES user_profiles (userId);
 
 #
 # INDEXEs
 #
+create index userId_ix
+    on user_profiles(userId);
 create index userFirstName_ix
     on user_profiles(firstName);
-
 create index userLastName_ix
     on user_profiles(lastName);
+create index userGender_ix
+    on user_profiles(gender);
 
 create index weight_ix
     on weight_records(weight);
 create index height_ix
     on height_records(height);
-create index neck_ix
-    on neck_length_records(neck_length);
+create index hip_ix
+    on hip_records(hip);
 create index waist_ix
-    on waist_length_records(waist_length);
+    on waist_records(waist);
