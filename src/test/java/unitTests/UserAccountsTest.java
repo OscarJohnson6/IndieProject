6d958967d1013bbffc3d0f2c34c12408aba68ef4
Oscar <ojohnson6@madisoncollege.edu>
2024-02-23 14:20:33 -0600

    Test class for user_profiles table

---
+++ src/test/java/unitTests/UserAccountsTest.java
@@ -0,0 +1,69 @@
+package unitTests;
+
+import fit.app.dao.User;
+import fit.app.database.UserDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.Database;
+
+import java.util.Date;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * The type Health numbers test.
+ *
+ * @author OscarJohnson6
+ */
+public class UserAccountsTest {
+
+    private User user;
+    private UserDao userDao;
+
+
+    /**
+     * Create a new ExerciseNinjas object before each test is run.
+     */
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+        userDao = new UserDao();
+    }
+
+    @Test
+    void testDatabase() {
+        user = new User(
+                "@email",
+                "Oscar",
+                "Johnson",
+                "male",
+                new Date());
+    }
+
+    @Test
+    void testDatabaseInsert() {
+        user = new User("Oscar@email");
+        int id = userDao.insert(user);
+        User userReturn = userDao.getById(id);
+
+        assertEquals(id, userReturn.getId());
+        assertEquals("Oscar@email", userReturn.getUserEmail());
+
+        // Weird failing test but has same result
+//        user.setId(id);
+//        assertSame(user, userReturn);
+    }
+
+    @Test
+    void testDatabaseUpdate() {
+        user = new User("Oscar@email");
+        userDao.update(user);
+    }
+
+    @Test
+    void testDatabaseAll() {
+        assertNotNull(userDao.getAll());
+    }
+}
