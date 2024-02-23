d67f6a2c9aa5d4d0e2f6840384f7e5d466098f13
Oscar <ojohnson6@madisoncollege.edu>
2024-02-23 14:27:11 -0600

    Test class for height records & set up for rest of record tables

---
+++ src/test/java/unitTests/RecordsTest.java
@@ -0,0 +1,80 @@
+package unitTests;
+
+import fit.app.dao.HeightRecord;
+import fit.app.dao.User;
+import fit.app.database.GenericDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import util.Database;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RecordsTest {
+
+    private HeightRecord heightRecord;
+    private GenericDao<HeightRecord> heightDao;
+
+    private User user;
+
+
+    /**
+     * Create a new ExerciseNinjas object before each test is run.
+     */
+    @BeforeEach
+    void setUp() {
+        Database database = Database.getInstance();
+        database.runSQL("cleandb.sql");
+        heightDao = new GenericDao<>(HeightRecord.class);
+    }
+
+    @Test
+    void testDatabase() {
+        heightRecord = new HeightRecord(123);
+    }
+
+    @Test
+    void testDatabaseInsert() {
+        user = new User(4);
+        heightRecord = new HeightRecord(user, 88);
+        int id = heightDao.insert(heightRecord);
+        heightRecord = heightDao.getById(id);
+
+        assertEquals(id, heightRecord.getId());
+        assertEquals(4, heightRecord.getUser().getId());
+        assertEquals(88, heightRecord.getHeight());
+    }
+
+    @Test
+    void testDatabaseUpdate() {
+        user = new User(1);
+        heightRecord = new HeightRecord(1, user, 112);
+        heightDao.update(heightRecord);
+        heightRecord = heightDao.getById(1);
+
+        assertEquals(1, heightRecord.getId());
+        assertEquals(1, heightRecord.getUser().getId());
+        assertEquals(112, heightRecord.getHeight());
+    }
+
+    @Test
+    void testDatabaseAll() {
+        assertNotNull(heightDao.getAll());
+    }
+
+    @Test
+    void testDatabaseDelete() {
+        user = new User(1);
+        heightRecord = new HeightRecord(2, user, 123);
+        heightDao.delete(heightRecord);
+
+        assertNull(heightDao.getById(2));
+    }
+
+    @Test
+    void testDatabaseDeleteAgain() {
+        heightRecord = heightDao.getById(3);
+        heightDao.delete(heightRecord);
+
+        assertNull(heightDao.getById(3));
+    }
+}
