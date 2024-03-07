2ed3ab4fae4aaeda256e839e0e2a50760bdd0855
Oscar <ojohnson6@madisoncollege.edu>
2024-03-07 03:24:24 -0600

    Added connection to user object & code for calculations

--- src/main/java/fit/app/api/HealthCalculations.java
+++ src/main/java/fit/app/api/HealthCalculations.java
@@ -1,6 +1,8 @@
 package fit.app.api;
 
-import fit.app.dao.User;
+import fit.app.dao.*;
+
+import java.util.List;
 
 /**
  * The type Health calculations.
@@ -8,6 +10,16 @@ import fit.app.dao.User;
 public class HealthCalculations {
 
     private User user;
+
+    // KG
+    private int weight;
+
+    // CM
+    private int height;
+
+    private int hip;
+
+    private int waist;
 
     /**
      * Instantiates a new Health calculations.
@@ -22,38 +34,58 @@ public class HealthCalculations {
      */
     public HealthCalculations(User user) {
         this.user = user;
+        List<WeightRecord> weightRecords = user.getWeightRecords();
+        List<HeightRecord> heightRecords = user.getHeightRecord();
+        List<HipRecord> hipRecords = user.getHipRecord();
+        List<WaistRecord> waistRecord = user.getWaistRecord();
+
+        if (!weightRecords.isEmpty()) {
+            weight = weightRecords.get(0).getWeight();
+        }
+
+        if (!heightRecords.isEmpty()) {
+            height = heightRecords.get(0).getHeight();
+        }
+
+        if (!hipRecords.isEmpty()) {
+            hip = hipRecords.get(0).getHip();
+        }
+
+        if (!waistRecord.isEmpty()) {
+            waist = waistRecord.get(0).getWaist();
+        }
     }
 
     /**
      * Gets bmi.
+     * BMI = weight (kg) / (height (m))^2
      *
      * @return the bmi
      */
-    public int getBMI() {
-        // BMI = weight (kg) / (height (m))^2
-        return 0;
+    public double getBMI() {
+        // Converting height from m to cm
+        int heightM = height / 100;
+        return (double) weight / (heightM * heightM);
     }
 
     /**
      * Gets whr.
+     * WHR = waist circumference / hip circumference
      *
      * @return the whr
      */
     public int getWHR() {
-        // WHR = waist circumference / hip circumference
         return 0;
     }
 
     /**
      * Gets bfp.
+     * For men: BMR = 88.362 + (13.397 × weight in kg) + (4.799 × height in cm) - (5.677 × age in years)
+     * For women: BMR = 447.593 + (9.247 × weight in kg) + (3.098 × height in cm) - (4.330 × age in years)
      *
      * @return the bfp
      */
     public int getBFP() {
-        /**
-         * For men: BMR = 88.362 + (13.397 × weight in kg) + (4.799 × height in cm) - (5.677 × age in years)
-         * For women: BMR = 447.593 + (9.247 × weight in kg) + (3.098 × height in cm) - (4.330 × age in years)
-         */
 
         return 0;
     }
@@ -70,49 +102,81 @@ public class HealthCalculations {
 
     /**
      * Gets tdee.
+     * BMR * activity_level (ex: BMR * 1.55)
      *
      * @return the tdee
      */
-    public int getTDEE() {
-        // BMR * activity_level (ex: BMR * 1.55)
-        return 0;
+    public double getTDEE() {
+        double effort = 0;
+
+        switch (user.getActivityLevel()) {
+            case "low":
+                effort = .5;
+                break;
+            case "medium":
+                effort = 1;
+                break;
+            case "high":
+                effort = 1.5;
+                break;
+        }
+
+        return getBMR() * effort;
     }
 
     /**
      * Gets ibw.
+     * For men: IBW = 50 + 2.3 × (height in inches - 60)
+     * For women: IBW = 45.5 + 2.3 × (height in inches - 60)
      *
      * @return the ibw
      */
-    public int getIBW() {
-        /**
-         * For men: IBW = 50 + 2.3 × (height in inches - 60)
-         * For women: IBW = 45.5 + 2.3 × (height in inches - 60)
-         */
+    public double getIBW() {
+        double IBW = 0;
+        double heightInches = height;
 
-        return 0;
+        switch (user.getGender()) {
+            case "male":
+                IBW = 50 + 2.3 * (heightInches - 60);
+                break;
+            case "female":
+                IBW = 45.5 + 2.3 * (heightInches - 60);
+                break;
+        }
+
+        return IBW;
     }
 
     /**
      * Gets lbm.
+     * For men: LBM = (0.407 × weight in kg) + (0.267 × height in cm) - 19.2
+     * For women: LBM = (0.252 × weight in kg) + (0.473 × height in cm) - 48.3
      *
      * @return the lbm
      */
-    public int getLBM() {
-        /**
-         * For men: LBM = (0.407 × weight in kg) + (0.267 × height in cm) - 19.2
-         * For women: LBM = (0.252 × weight in kg) + (0.473 × height in cm) - 48.3
-         */
+    public double getLBM() {
+        double LBM = 0;
+        double heightInches = height;
 
-        return 0;
+        switch (user.getGender()) {
+            case "male":
+                LBM = (0.407 * weight) + (0.267 * height) - 19.2;
+                break;
+            case "female":
+                LBM = (0.252 * weight) + (0.473 * height) - 48.3;
+                break;
+        }
+
+        return LBM;
     }
 
     /**
      * Gets whtr.
+     * WHtR = waist circumference / height
      *
      * @return the whtr
      */
-    public int getWHTR() {
-        // WHtR = waist circumference / height
-        return 0;
+    public double getWHTR() {
+        return (double) waist / height;
     }
 }
