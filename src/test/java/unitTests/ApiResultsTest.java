1194512620151f60fe06006d77945cc0912a7dc9
Oscar <ojohnson6@madisoncollege.edu>
2024-03-10 00:46:34 -0600

    Passing unit test on Json response from ExerciseDb API

--- src/test/java/unitTests/ApiResultsTest.java
+++ src/test/java/unitTests/ApiResultsTest.java
@@ -1,14 +1,17 @@
 package unitTests;
 
-import fit.app.api.ExerciseNinjas;
+import fit.app.api.ApiNinjas;
+import fit.app.api.ExerciseDb;
+import fit.app.pojo.ExerciseDbJson;
 import org.junit.jupiter.api.BeforeEach;
+import org.testng.annotations.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * The type Api results test.
@@ -27,7 +30,7 @@ public class ApiResultsTest {
      */
 //    @Test
     void testAPIResultsAll() {
-        ExerciseNinjas ninjasExercise = new ExerciseNinjas();
+        ApiNinjas ninjasExercise = new ApiNinjas();
         List<TreeMap<String, String>> result = ninjasExercise.createApiResponse(
                 "",
                 "",
@@ -38,5 +41,37 @@ public class ApiResultsTest {
         assertNotNull(result);
         assertFalse(result.isEmpty());
         assertEquals(10, result.size());
+    }
+
+    /**
+     * CAUTION: This is connected to a limited (500 uses) API.
+     */
+//    @Test
+    void testExerciseDb() {
+        ExerciseDb exerciseDb = new ExerciseDb();
+        ArrayList<ExerciseDbJson> exerciseArray = exerciseDb.createApiResponse(0, 0);
+
+        assertNotNull(exerciseArray);
+        assertEquals(10, exerciseArray.size());
+
+        ExerciseDbJson exercise0 = exerciseArray.get(0);
+        assertEquals("waist", exercise0.getBodyPart());
+        assertEquals("body weight", exercise0.getEquipment());
+        assertEquals("3/4 sit-up", exercise0.getName());
+        assertEquals("abs", exercise0.getTarget());
+        assertEquals(testInstructions(), exercise0.getInstructions());
+
+    }
+
+    private List<String> testInstructions() {
+        List<String> testList = new ArrayList<>();
+        testList.add("Lie flat on your back with your knees bent and feet flat on the ground.");
+        testList.add("Place your hands behind your head with your elbows pointing outwards.");
+        testList.add("Engaging your abs, slowly lift your upper body off the "
+                + "ground, curling forward until your torso is at a 45-degree angle.");
+        testList.add("Pause for a moment at the top, then slowly lower your "
+                + "upper body back down to the starting position.");
+        testList.add("Repeat for the desired number of repetitions.");
+        return testList;
     }
 }
