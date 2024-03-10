19c7d1553240e4fc987ee262801ee7697b57c1d1
Oscar <ojohnson6@madisoncollege.edu>
2024-03-10 00:58:12 -0600

    Basic servlets for linking to JSPs

---
+++ src/main/java/fit/app/controller/ExerciseDbServlet.java
@@ -0,0 +1,39 @@
+package fit.app.controller;
+
+import fit.app.api.ExerciseDb;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * A simple servlet.
+ *
+ * @author OscarJohnson6
+ */
+@WebServlet(
+        urlPatterns = {"/ExerciseDB"}
+)
+public class ExerciseDbServlet extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ExerciseDb exercise = new ExerciseDb();
+
+
+        req.setAttribute("exerciseDbJson", exercise.createApiResponse(
+                Integer.parseInt(req.getParameter("limitExerciseDb")),
+                Integer.parseInt(req.getParameter("offsetExerciseDb"))));
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/exerciseDbResults.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
---
+++ src/main/java/fit/app/controller/IndexServlet.java
@@ -0,0 +1,40 @@
+package fit.app.controller;
+
+import fit.app.dao.*;
+import fit.app.database.GenericDao;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+/**
+ * A simple servlet.
+ *
+ * @author OscarJohnson6
+ */
+@WebServlet(
+        urlPatterns = {"/settings"},
+        loadOnStartup = 1
+)
+public class IndexServlet extends HttpServlet {
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // TODO delete user below
+        GenericDao<User> userDao = new GenericDao<>(User.class);
+        User user = userDao.getById(1);
+        logger.info(user);
+        req.setAttribute("user", user);
+        logger.info(req.getAttribute("user"));
+        RequestDispatcher dispatcher = req.getRequestDispatcher("index.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
