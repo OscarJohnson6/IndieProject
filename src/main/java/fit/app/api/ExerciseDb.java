acfe7e0763da5953a9e59dae45906fb95f2e3cce
Oscar <ojohnson6@madisoncollege.edu>
2024-03-10 01:12:48 -0600

    API ExerciseDb connection class & response formatter

---
+++ src/main/java/fit/app/api/ExerciseDb.java
@@ -0,0 +1,75 @@
+package fit.app.api;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import fit.app.pojo.ExerciseDbJson;
+import fit.app.utilities.PropertiesLoader;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Properties;
+
+public class ExerciseDb implements PropertiesLoader {
+    /**
+     * The Logger.
+     */
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+    /**
+     * The Properties.
+     */
+    private final Properties properties = loadProperties("/api.properties");
+
+    /**
+     * Create api response map.
+     *
+     * @param limit       the limit
+     * @param offset     the offset
+     */
+    public ArrayList<ExerciseDbJson> createApiResponse(int limit, int offset) {
+        String url = properties.getProperty("exercise.db.url");
+
+        if (limit != 0) {
+            url += "&limit=" + limit;
+        }
+        if (offset != 0) {
+            url += "&offset=" + offset;
+        }
+
+        return generateResponse(url);
+    }
+
+    /**
+     * Generate response object with ExerciseDbJson.
+     *
+     * @param url the url
+     */
+    private ArrayList<ExerciseDbJson> generateResponse(String url) {
+        OkHttpClient client = new OkHttpClient();
+        ArrayList<ExerciseDbJson> list = null;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .get()
+                .addHeader("X-RapidAPI-Key", properties.getProperty("exercise.db.key"))
+                .addHeader("X-RapidAPI-Host", properties.getProperty("exercise.db.host"))
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.body() != null) {
+                String stringResponse = response.body().string();
+                ObjectMapper mapper = new ObjectMapper();
+                list = mapper.readValue(stringResponse, new TypeReference<>() {});
+            }
+        } catch (IOException ioException) {
+            logger.error("Problem reading JSON in generateResponse() ", ioException);
+        }
+
+        return list;
+    }
+}
