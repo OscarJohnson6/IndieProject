38240ec4b9880cc6a13c458a13cc6072c7889c4b
Oscar <ojohnson6@madisoncollege.edu>
2024-04-02 20:33:51 -0500

    Servlet to remove session attribute & sign out a user

---
+++ src/main/java/fit/app/controller/SignOutServlet.java
@@ -0,0 +1,30 @@
+package fit.app.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+/**
+ * A simple servlet.
+ *
+ * @author OscarJohnson6
+ */
+@WebServlet(
+        urlPatterns = {"/signOut"}
+)
+public class SignOutServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        if (session.getAttribute("userAccount") != null) {
+            session.removeAttribute("userAccount");
+        }
+    }
+}
